1e7bedfc80a20ac6f895b31794060fa88fc476f6
hnakano863 <notchi863@gmail.com>
2021-04-09 16:53:32 +0900

    fix missusing of buildPythonPackages

--- python/default.nix
+++ python/default.nix
@@ -1,11 +1,15 @@
-final: prev: {
+final: prev:
+let
+  buildPythonPackage = prev.python38Packages.buildPythonPackages;
+in
+{
 
-  autograd-gamma = final.callPackage ./autograd-gamma { };
+  autograd-gamma = final.callPackage ./autograd-gamma { inherit buildPythonPackages; };
 
-  formulaic = final.callPackage ./formulaic { };
+  formulaic = final.callPackage ./formulaic { inherit buildPythonPackages; };
 
-  interface-meta = final.callPackage ./interface-meta { };
+  interface-meta = final.callPackage ./interface-meta { inherit buildPythonPackages; };
 
-  lifelines = final.callPackage ./lifelines { };
+  lifelines = final.callPackage ./lifelines { inherit buildPythonPackages; };
 
 }
