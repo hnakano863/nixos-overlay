2c2ee033fd729930f1bc2ca8ee958dd6a0e72e46
hnakano863 <notchi863@gmail.com>
2021-06-14 11:53:01 +0900

    bump

--- python/lifelines/default.nix
+++ python/lifelines/default.nix
@@ -13,11 +13,11 @@
 
 buildPythonPackage rec {
   pname = "lifelines";
-  version = "0.25.10";
+  version = "0.26.0";
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "7e2df924e5c6725550e520da8ece9a46322eba6fbd00e8c7ddb46a01cedf1559";
+    sha256 = "e9b3e45a81e4933aa5277168242d7f88b7e374d26e90aa6fb1242a08a8f50332";
   };
 
   propagatedBuildInputs = [
