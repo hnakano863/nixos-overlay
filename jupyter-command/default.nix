0492663ae175d53ae7c024c6c74e5258297d9af8
hnakano863 <notchi863@gmail.com>
2021-06-16 23:40:36 +0900

    JUPYTER_PATH環境変数を使えるようにmakeWrapperArgsを外す

--- jupyter-command/default.nix
+++ jupyter-command/default.nix
@@ -1,37 +1,7 @@
 { pkgs }:
 with pkgs;
 let
-
-  env = python3.withPackages (p: with p; [
-    ipykernel numpy pandas matplotlib
-  ]);
-
-  definitions.python3 = {
-    displayName = "Python3";
-    argv = [
-      env.interpreter
-      "-m"
-      "ipykernel_launcher"
-      "-f"
-      "{connection_file}"
-    ];
-    language = "python";
-    logo32 = "${env.sitePackages}/ipykernel/resources/logo-32x32.png";
-    logo64 = "${env.sitePackages}/ipykernel/resources/logo-64x64.png";
-  };
-
-  jupyterPath = jupyter-kernel.create { inherit definitions; };
-
-  packageOverrides = final: prev: {
-    jupyter_core = prev.jupyter_core.overridePythonAttrs (old: {
-      makeWrapperArgs = [ "--set JUPYTER_PATH ${jupyterPath}" ];
-    });
-  };
-
-  jupyterApp = let
-    myPython = (python3.override { inherit packageOverrides; });
-  in myPython.withPackages (ps: with ps; [ jupyter jupyterlab ]);
-
+  jupyterApp = python3.withPackages (ps: with ps; [ jupyter jupyterlab ]);
 in stdenvNoCC.mkDerivation {
   name = "jupyter-command";
   phases = [ "installPhase" ];
